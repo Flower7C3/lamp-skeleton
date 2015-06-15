de0eb3907be6929db0261af9eec35dc1ebe7ae17
Bartłomiej Kwiatek <bartlomiej.kwiatek@vml.com>
2015-06-15 12:22:14 +0200

    Add filter input. Move navigation to navbar.

--- domains/default/index.php
+++ domains/default/index.php
@@ -106,58 +106,69 @@ switch ($_GET['d']) {
         <meta charset="UTF-8">
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
-        <script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
-        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
     </head>
     <body>
-    <section class="container">
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h3 class="panel-title">
-                            <span class="glyphicon glyphicon-certificate"></span>
-                            VServer
-                        </h3>
-                    </div>
-                    <? if(empty($domains)): ?>
-                        <div class="panel-body">
-                            <em class="glyphicon glyphicon-info"></em>
-                            No directories configured. Create one with suffix <code><?= stripslashes($suffix) ?></code> in <code><?= $dir ?></code> on virtual machine and add it to hosts file in local machine.
-                        </div>
-                    <? else: ?>
-                        <div class="panel-body">
-                            <div class="btn-group" role="group">
-                                <? foreach ($orders as $order): ?>
-                                    <a class="btn btn-default<? if ($_GET['k'] == $order->key AND $_GET['d'] == $order->direction): ?> active<? endif; ?>" href="?k=<?= $order->key ?>&d=<?= $order->direction ?>&t=<?= $_GET['t'] ?>">
+        <nav class="navbar navbar-default">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="/">
+                        <span class="glyphicon glyphicon-fire"></span>
+                        VServer
+                    </a>
+                </div>
+                <? if (!empty($domains)): ?>
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav">
+                            <? foreach ($orders as $order): ?>
+                                <li<? if ($_GET['k'] == $order->key AND $_GET['d'] == $order->direction): ?> class="active"<? endif; ?>>
+                                    <a href="?k=<?= $order->key ?>&d=<?= $order->direction ?>&t=<?= $_GET['t'] ?>">
                                         <span class="glyphicon glyphicon-<?= $order->icon ?>"></span>
                                         <span class="glyphicon glyphicon-chevron-<?= ($order->direction == "asc") ? "up" : "down" ?>"></span>
+                                        <span class="hidden-sm hidden-md hidden-lg">Order by <?= $order->key ?> <?= $order->direction ?></span>
                                     </a>
-                                <? endforeach; ?>
+                                </li>
+                            <? endforeach; ?>
+                        </ul>
+                        <form class="navbar-form navbar-left" role="search">
+                            <div class="form-group">
+                                <div class="form-group has-feedback">
+                                    <span class="glyphicon glyphicon-filter form-control-feedback" aria-hidden="true"></span>
+                                    <input class="form-control" id="searchinput" type="search" placeholder="Filter by domain..."/>
+                                </div>
                             </div>
-                            <div class="btn-group pull-right" role="group">
-                                <a class="btn btn-default" data-toggle="modal" data-target="#myModal">
+                        </form>
+                        <ul class="nav navbar-nav navbar-right">
+                            <li>
+                                <a data-toggle="modal" href="#hostsConfig">
                                     <span class="glyphicon glyphicon-cog"></span>
+                                    <span class="hidden-sm hidden-md hidden-lg">Show hosts config</span>
                                 </a>
 
-                                <div class="modal fade" id="myModal">
-                                    <div class="modal-dialog modal-sm">
+                                <div class="modal fade" id="hostsConfig">
+                                    <div class="modal-dialog modal-lg">
                                         <div class="modal-content">
                                             <div class="modal-header">
                                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                                                 <h4 class="modal-title">Hosts config</h4>
                                             </div>
                                             <div class="modal-body">
-                                                <code>
-                                                    <?= $_SERVER['SERVER_ADDR'] ?>
-                                                    <?= implode("\t", $hosts) ?>
-                                                </code>
+                                                Copy following code and paste into <code>/etc/hosts</code> file.
+                                                <textarea style="width: 100%;height: 400px; resize: none"><?= $_SERVER['SERVER_ADDR'] . "\t" . implode("\t", $hosts) ?></textarea>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                                <a class="btn btn-default dropdown-toggle<? if (!empty($_GET['t'])): ?> active<? endif; ?>" data-toggle="dropdown">
+                            </li>
+                            <li class="dropdown<? if (!empty($_GET['t'])): ?> active<? endif; ?>">
+                                <a class="dropdown-toggle" data-toggle="dropdown">
                                     <span class="glyphicon glyphicon-tag"></span>
+                                    <span class="hidden-sm hidden-md hidden-lg">Show tags</span>
                                     <span class="caret"></span>
                                 </a>
                                 <ul class="dropdown-menu dropdown-menu-right" role="menu">
@@ -177,24 +188,45 @@ switch ($_GET['d']) {
                                         </li>
                                     <? endforeach; ?>
                                 </ul>
+                            </li>
+                        </ul>
+                    </div>
+                <? endif; ?>
+            </div>
+        </nav>
 
-                            </div>
+        <section class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <? if (empty($domains)): ?>
+                        <div class="alert alert-info">
+                            <em class="glyphicon glyphicon-info-sign"></em>
+                            No directories configured. Create one with suffix <code><?= stripslashes($suffix) ?></code> in <code><?= $dir ?></code> on virtual machine and add it to hosts file in local machine.
                         </div>
-                        <div class="list-group">
-                            <? foreach ($domains as $domain): ?>
-                                <a href="<?= $domain->url ?>" class="list-group-item">
-                                    <? foreach ($domain->tags as $tag): ?>
-                                        <span class="text"><?= $tag ?></span>
-                                    <? endforeach ?>
-                                    <!--                            <span class="text">--><? //= $domain->name ?><!--</span>-->
-                                    <span class="label label-success"><?= $domain->code ?></span>
-                                    <span class="badge"><?= $domain->date ?></span>
-                                </a>
-                            <? endforeach ?>
+                    <? else: ?>
+                        <div class="panel panel-default">
+                            <div class="list-group" id="searchlist">
+                                <? foreach ($domains as $domain): ?>
+                                    <a href="<?= $domain->url ?>" class="list-group-item">
+                                        <? foreach ($domain->tags as $tag): ?>
+                                            <span class="text"><?= $tag ?></span>
+                                        <? endforeach ?>
+                                        <span class="hidden name"><?= $domain->name ?></span>
+                                        <span class="label label-success"><?= $domain->code ?></span>
+                                        <span class="badge"><?= $domain->date ?></span>
+                                    </a>
+                                <? endforeach ?>
+                            </div>
                         </div>
                     <? endif; ?>
                 </div>
             </div>
-        </div>
+        </section>
+        <script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+        <script src="//labs.easyblog.it/bootstrap-list-filter/bootstrap-list-filter.src.js"></script>
+        <script>
+            $('#searchlist').btsListFilter('#searchinput', {itemChild: '.name', initial: false});
+        </script>
     </body>
 </html>
